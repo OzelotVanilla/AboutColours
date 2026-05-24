cd16a08020825ecc43bae4564bc4fe82c1bef5f1
Ozelot Vanilla <ozelotvanilla@gmail.com>
2026-05-24 11:19:52 +0900

    prefab - `Stage`: `01` to `03`
    This is a temp solution for this project.
    
    In the future, the stage will be saved as text format,
     and load from it.

---
+++ Assets/src/prefabs/stages/Stage01.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Stage01 : MonoBehaviour
+{
+    void __start__() { }
+
+    void __update__() { }
+
+    void Start() { this.__start__(); }
+
+    void Update() { this.__update__(); }
+}
---
+++ Assets/src/prefabs/stages/Stage02.cs
@@ -0,0 +1,12 @@
+using UnityEngine;
+
+public class Stage02 : MonoBehaviour
+{
+    void __start__() { }
+
+    void __update__() { }
+
+    void Start() { this.__start__(); }
+
+    void Update() { this.__update__(); }
+}
